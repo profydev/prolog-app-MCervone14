db111f6da5e3a20b5bb8f854aaa060b85b7c255c
Michael Cervone <MCervone14@gmail.com>
2023-11-28 15:52:17 -0600

    Implemented Footer with Cypress tests

---
+++ cypress/e2e/footer.cy.ts
@@ -0,0 +1,59 @@
+import { version as app_version } from "../../package.json";
+
+describe("Footer", () => {
+  it("Displays on each page", () => {
+    cy.viewport(1025, 900);
+    cy.visit("http://localhost:3000/dashboard");
+    cy.get("footer").should("exist");
+
+    cy.visit("http://localhost:3000/dashboard/issues");
+    cy.get("footer").should("exist");
+
+    cy.visit("http://localhost:3000/dashboard/alerts");
+    cy.get("footer").should("exist");
+
+    cy.visit("http://localhost:3000/dashboard/users");
+    cy.get("footer").should("exist");
+
+    cy.visit("http://localhost:3000/dashboard/settings");
+    cy.get("footer").should("exist");
+  });
+
+  it("Displays the correct text and logo", () => {
+    cy.viewport(1025, 900);
+    cy.visit("http://localhost:3000/dashboard");
+    cy.wait(1000);
+    cy.get("footer")
+      .find("div")
+      .first()
+      .should("contain.text", `Version: ${app_version}`)
+      .and("have.css", "fontFamily", "Inter, sans-serif")
+      .and("have.css", "fontWeight", "400")
+      .and("have.css", "fontSize", "16px")
+      .and("have.css", { lineHeight: "24px" });
+
+    cy.get("footer")
+      .find("nav")
+      .find("ul")
+      .find("li")
+      .each(($el, index) => {
+        const items = ["Docs", "API", "Help", "Community"];
+        cy.wrap($el)
+          .should("have.text", items[index])
+          .and("have.css", "fontFamily", "inter, sans-serif")
+          .and("have.css", "fontWeight", "500")
+          .and("have.css", "fontSize", "16px")
+          .and("have.css", "lineHeight", "24px");
+      })
+      .find("a")
+      .each(($el) => {
+        cy.wrap($el).should("have.attr", "href", "/dashboard#");
+      });
+
+    cy.get("footer")
+      .find("img")
+      .should("have.attr", "src", "./icons/logo-small.svg")
+      .and("have.css", "width", "23px")
+      .and("have.css", "height", "33px");
+  });
+});
